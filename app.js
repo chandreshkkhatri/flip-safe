9a7889db7d4c2e884017e2abebb3945d7b21f9c3
Gerdent Baker <gerdent@protonmail.com>
2019-08-04 18:16:15 +0530

    local changes

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors')
@@ -8,16 +7,11 @@ var cors = require('cors')
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-///////////~
 const KiteConnect = require("kiteconnect").KiteConnect;
 const alertsRouter = require('./routes/alerts-router')
 const authRouter = require('./routes/auth-router')
@@ -27,8 +21,6 @@ const tickerRouter = require('./routes/ticker-router')
 app.locals.cred = require('./app-cred.json')
 app.locals.reset = () => { app.locals.kc = new KiteConnect({ api_key: app.locals.cred.api_key }); }
 app.locals.reset()
-//////////////!
-//////////////~
 app.use('/alerts', alertsRouter)
 app.use('/auth', authRouter)
 app.use('/db', dbRouter)
